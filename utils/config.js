0fe48806599bd4d63b96e032d586d0ff69992792
namelzx <914057262@qq.com>
2019-12-18 12:52:05 +0800

    更新图片地址

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,6 @@
 const config = {
   api_blink_url: 'https://aa.8yueguihua.com/api/',
-  api_images_url: 'https://www.zmjg.net.cn/',
+  api_images_url: 'https://aa.8yueguihua.com/api/',
 }
 
 export { config }
