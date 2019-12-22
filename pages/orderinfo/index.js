4fb50d2ee9bf7cd7700a5fcebc21f3aec8556a16
namelzx <914057262@qq.com>
2019-12-22 13:42:48 +0800

    更新订单详情字符处理

--- pages/orderinfo/index.js
+++ pages/orderinfo/index.js
@@ -1,10 +1,23 @@
 // pages/orderinfo/index.js
+
+var util = require("../../utils/Time.js")
+
+const app = getApp();
+const base_path = '../../images/home/';
+// 这是该页面对应的 “路径前缀”
+import {
+  OrderModel
+} from '../../api/order.js';
+let list = new OrderModel();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    myorder:{},
+    bar_url:'',
+    id:'',
 
   },
 
@@ -12,7 +25,10 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+      console.log(options)
+      this.setData({
+        id:options.id
+      })
   },
 
   /**
@@ -26,7 +42,27 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
+    var that = this;
+    var id = this.data.id;
+    var bar_url = app.globalData.bar_url;
+    list.postOrderpay(id, res => {
 
+      var in_titme = util.getDates(1, util.formatDate(new Date(res.inTime * 1000)));
+      console.log(in_titme)
+      res.in_time = in_titme[0].week
+      res.inTime = in_titme[0].time
+      var out_Time = util.getDates(1, util.formatDate(new Date(res.outTime * 1000)));
+      
+      res.out_time = out_Time[0].week
+      res.outTime = out_Time[0].time
+      that.setData({
+        myorder: res
+      })
+      console.log(that.data.myorder)
+    })
+    that.setData({
+      bar_url: bar_url
+    })
   },
 
   /**
