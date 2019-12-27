cd4af6ea3850aadade4b304dc5c852423cf20cf5
狗剩先生 <1056040932@qq.com>
2019-12-27 22:26:45 +0800

    增加评论页

---
+++ pages/component/comment-bar/index.js
@@ -0,0 +1,23 @@
+// pages/component/comment-bar/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    star:5
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
